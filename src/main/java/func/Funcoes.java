897f1ecb7aa3b06c0c19920b42aebc5a0b007853
felipeS5 <felipe5smash@gmail.com>
2023-01-30 09:33:32 -0300

    Terefas armazenadas, faltam status

--- src/main/java/func/Funcoes.java
+++ src/main/java/func/Funcoes.java
@@ -6,22 +6,32 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 import storage.Dados;
+import storage.ReadWrite;
 
 public class Funcoes {
 	private Scanner sc = new Scanner(System.in);
 	private List<Dados> lista = new ArrayList<>();
+	private ReadWrite rw = new ReadWrite();
 	
+	public Funcoes() {
+		lista = rw.ler(lista);
+	}
+	
+	// Adiciona
 	public void add() {
 		limpaTela();
 		System.out.print("\n\u001b[96mInsira a tarefa:\n>\u001b[0m ");
 		lista.add(new Dados(sc.nextLine()));
+		salvar();
 	}
 	
+	// Remoção
 	public void remove() {
 		if (lista.size() > 0) {
 			System.out.print("\u001b[96mQual tarefa deve ser removida?\n>\u001b[0m ");
 			try{
 				lista.remove(sc.nextInt());
+				salvar();
 				sc.nextLine();
 			}catch (IndexOutOfBoundsException e) {
 				System.out.println("\n\u001b[43m\u001b[30mEsta tarefa não existe!\u001b[0m\n");
@@ -38,6 +48,11 @@ public class Funcoes {
 		}
 	}
 	
+	public void salvar() {
+		rw.escrever(lista);
+	}
+	
+	// Visualização
 	public void ver() {
 		limpaTela();
 		System.out.println("Tarefas:\n");
@@ -46,6 +61,7 @@ public class Funcoes {
 			System.out.println(i+1 + "- " + lista.get(i) + "\n");
 		}
 	}
+	
 	
 	public void limpaTela() {
 		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
--- src/main/java/storage/ReadWrite.java
+++ src/main/java/storage/ReadWrite.java
@@ -1,5 +1,42 @@
 package storage;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Scanner;
+
 public class ReadWrite {
+	private File arq = new File("src/main/java/storage/dados.txt");
+	private Scanner leitor;
+	private FileWriter escreve;
 	
+	// Leitura
+	public List ler(List lista) {
+		
+		try{
+			leitor = new Scanner(arq);
+		}catch(Exception e) {}
+		
+		while (leitor.hasNextLine()) {
+			lista.add(leitor.nextLine());
+		}
+		
+		leitor.close();
+		
+		return lista;
+	}
+	
+	// Escrita
+	public void escrever(List lista) {
+		try{
+			escreve = new FileWriter(arq);
+			String dados = "";
+			for ( int i=0; i<lista.size(); i++) {
+				dados += lista.get(i) + "\n";
+			}
+			
+			escreve.write(dados);
+			escreve.close();
+		} catch (Exception e){}
+	}
 }
